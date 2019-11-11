70809164ff1428d16cc58caa8e37e34b1d8b017c
softtron <335961289@qq.com>
2019-11-11 17:14:36 +0800

    777

--- src/main/java/com/softtron/pinmaoserver/controllers/IndexController.java
+++ src/main/java/com/softtron/pinmaoserver/controllers/IndexController.java
@@ -2,6 +2,6 @@ package com.softtron.pinmaoserver.controllers;
 
 public class IndexController {
 	public static void main(String[] args) {
-		System.out.println("Hello worldSSS!");
+		System.out.println("Hello world777!");
 	}
 }
